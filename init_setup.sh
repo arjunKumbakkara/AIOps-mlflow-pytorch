ab623059e62ed5a228368f4b842c6948bbb1b86d
arjunkumbakkara <arjunkumbakkara@gmail.com>
2022-02-24 13:24:46 +0530

    Setup Script + changes

---
+++ init_setup.sh
@@ -0,0 +1,5 @@
+conda create --prefix ./env python=3.7 -y 
+conda init
+source activate ./env
+pip install torch==1.10.2+cpu torchvision==0.11.3+cpu torchaudio==0.10.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+pip install -r requirements.txt
