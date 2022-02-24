53fa9fd83f115eb7e2e1c1e289ac4f9d23e7a691
arjunkumbakkara <arjunkumbakkara@gmail.com>
2022-02-24 13:33:31 +0530

    Changes

--- init_setup.sh
+++ init_setup.sh
@@ -3,3 +3,5 @@ conda init
 source activate ./env
 pip install torch==1.10.2+cpu torchvision==0.11.3+cpu torchaudio==0.10.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
 pip install -r requirements.txt
+
+
