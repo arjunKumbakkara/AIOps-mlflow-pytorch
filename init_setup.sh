1cae38a053e32cd7865dd2c4aff13a3ce9e267a0
arjunkumbakkara <arjunkumbakkara@gmail.com>
2022-02-25 09:00:36 +0530

    Initial Setup added

--- init_setup.sh
+++ init_setup.sh
@@ -1,7 +1,12 @@
 conda create --prefix ./env python=3.7 -y 
 conda init
+
+#in Windows, if you want then, use  
+#source C:user/arjunkumbakkara/..../conda.sh   -- this makes conda avaialable globally
+#conda activate ./env
+
 source activate ./env
-pip install torch==1.10.2+cpu torchvision==0.11.3+cpu torchaudio==0.10.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html
+pip install torch==1.10.2+cpu torchvision==0.11.3+cpu torchaudio==0.10.2+cpu -f https://download.pytorch.org/whl/cpu/torch_stable.html -y
 pip install -r requirements.txt
 
 
